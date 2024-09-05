7b315b45ec238dfb86e2815107574e9964660d48
Minseo Kim <kimminss0@outlook.kr>
2024-09-05 22:08:16 +0900

    feat: support LaTeX & improve design

--- src/Main.hs
+++ src/Main.hs
@@ -2,12 +2,15 @@
 {-# LANGUAGE OverloadedStrings #-}
 import           Data.Monoid (mappend)
 import           Hakyll
-
+import Text.Pandoc
+import Text.Pandoc.Options
+import Hakyll.Web.Pandoc
+import qualified Data.Set as S
 
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
-    match "images/*" $ do
+    match ("images/*" .||. "js/*" .||. "css/fonts/*") $ do
         route   idRoute
         compile copyFileCompiler
 
@@ -23,9 +26,9 @@ main = hakyll $ do
 
     match "posts/*" $ do
         route $ setExtension "html"
-        compile $ pandocCompiler
+        compile $ pandocCompilerWith readerOpts writerOpts
             >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
+            >>= loadAndApplyTemplate "templates/default.html" (mathCtx `mappend` postCtx)
             >>= relativizeUrls
 
     create ["archive.html"] $ do
@@ -64,3 +67,21 @@ postCtx :: Context String
 postCtx =
     dateField "date" "%B %e, %Y" `mappend`
     defaultContext
+
+readerOpts :: ReaderOptions
+readerOpts = defaultHakyllReaderOptions
+
+writerOpts :: WriterOptions
+writerOpts = defaultHakyllWriterOptions {
+    writerHTMLMathMethod = MathJax ""
+}
+
+mathCtx :: Context a
+mathCtx = field "katex" $ \item -> do
+    katex <- getMetadataField (itemIdentifier item) "katex"
+    return $ case katex of
+                    Just "false" -> ""
+                    Just "off" -> ""
+                    _ -> "<link rel=\"stylesheet\" href=\"/css/katex.min.css\">\n\
+                             \<script type=\"text/javascript\" src=\"/js/katex.min.js\"></script>\n\
+                             \<script src=\"/js/auto-render.min.js\"></script>"
